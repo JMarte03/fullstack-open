ed291f9ff2d227e0732e3ff47d58a0a7f7187583
JMarte03 <jesimielmarte.dev@gmail.com>
2025-07-10 15:49:03 -0400

    Exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,6 +4,8 @@ function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [collected, setCollected] = useState(0);
+  const [average, setAverage] = useState(0);
 
   const handleGood = () => {
     const updatedGood = good + 1
@@ -17,6 +19,14 @@ function App() {
     const updatedBad = bad + 1
     setBad(updatedBad)
   }
+
+  const calculateCollected = (n1, n2, n3) => {
+    return n1 + n2 + n3
+  }
+
+  const calculateAverage = (n1, n2, n3) => {
+    return ((n1 + (n3*-1))/(n1+n2+n3))
+  }
   return (
     <div>
       <h1>give feedback</h1>
@@ -27,6 +37,9 @@ function App() {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>Collected feedback: {calculateCollected(good, neutral, bad)}</p>
+      <p>Average: {calculateAverage(good, neutral, bad)}</p>
+      <p>Positive feedback: {(good/calculateCollected(good, neutral, bad))*100}%</p>
     </div>
   )
 }
