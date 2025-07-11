132f22a0551d6169a42bfd55d3381ae731280999
JMarte03 <jesimielmarte.dev@gmail.com>
2025-07-11 12:53:32 -0400

    Exercises 2.2 and 2.3

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -21,12 +21,22 @@ const Part = ({ part }) => {
     return <p>{part.name} {part.exercises}</p>
 }
 
+// Calculates the sum of all the exercises properties in each part with the reduce method
+const Total = ({ parts }) => {
+    const total = parts.reduce((acc, part) => {
+        // console.log(part.exercises);
+        return acc + part.exercises
+    }, 0)
+    return <p>Number of exercises {total}</p>
+}
+
 // The whole course component
 const Course = ({ course }) => {
   return (
     <div>
       <Header text={course.name} />
       <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
