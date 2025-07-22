6feceabd6c97b9d1b36e376b078634b8dde3e4e3
JMarte03 <jesimielmarte.dev@gmail.com>
2025-07-22 11:42:15 -0400

    Exercise 3.7

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let persons = [
     { 
@@ -93,6 +95,14 @@ app.get('/info', (request, response) => {
     const today = new Date()
     response.send(`<p>Phonebook has info for ${persons.length} people</p> <p>${today}</p>`)
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({
+        error: 'unknown endpoint'
+    })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT)
